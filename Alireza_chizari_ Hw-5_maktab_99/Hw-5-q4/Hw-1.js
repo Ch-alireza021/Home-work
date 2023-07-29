8f518014941d65d5e74f388817cc9a4e49647013
sasan <137183548+Ch-alireza021@users.noreply.github.com>
2023-07-29 21:18:22 +0330

    editing Hw-5-q3

--- Alireza_chizari_ Hw-5_maktab_99/Hw-5-q4/Hw-1.js
+++ Alireza_chizari_ Hw-5_maktab_99/Hw-5-q4/Hw-1.js
@@ -9,8 +9,6 @@
 //     form.reset()
 // })
 
-
-
 // const topOrBottom=function(formData){
 //     const distance1=formData.distance1
 //     console.log(distance1);
@@ -31,38 +29,46 @@
 
 // }
 
+const form = document.querySelector("#form");
+const toast = document.querySelector("#toast");
+const submit = document.querySelector("#submit");
 
-const form=document.querySelector('#form');
-const toast=document.querySelector('#toast');
-const submit=document.querySelector('#submit')
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(form).entries());
+  console.log(formData);
+  tost(formData);
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 3000);
+  // form.reset()
+});
 
-submit.addEventListener('click',function(e){
-    e.preventDefault();
-    const formData=Object.fromEntries(new FormData(form).entries())
-    console.log(formData);
-    tost(formData)
-    // form.reset()
-})
+let topOrBottom = function (formData) {
+  let distance1 = formData.distance1;
+  console.log(distance1);
+  let distance2 = formData.distance2;
+  console.log(distance2);
+  console.log(formData.loc1);
+  console.log(formData.loc2);
 
+  toast.style.top = "";
+  toast.style.bottom = "";
+  toast.style.left = "";
+  toast.style.right = "";
 
+  formData.loc1 == "top"
+    ? (toast.style.top = `${distance1}px`)
+    : (toast.style.bottom = `${distance1}px`);
+  formData.loc2 == "left"
+    ? (toast.style.left = `${distance2}px`)
+    : (toast.style.right = `${distance2}px`);
+};
 
-let topOrBottom=function(formData){
-    let distance1=formData.distance1
-    console.log(distance1);
-    let distance2=formData.distance2
-    console.log(distance2);
-console.log(formData.loc1);
-console.log(formData.loc2);
-    formData.loc1==top ? toast.style.top=`${distance1}px`
-    : toast.style.bottom=`${distance1}px`;
-    formData.loc2==left ? toast.style.left=`${distance2}px`
-    : toast.style.right=`${distance2}px`;
-}
-
-const tost=function(formData){
-    const showMassage=formData.showMassage
-    console.log(showMassage);
-    toast.textContent=showMassage
-    topOrBottom(formData)
-
-}
+const tost = function (formData) {
+  const showMassage = formData.showMassage;
+  console.log(showMassage);
+  toast.style.display = "block";
+  toast.textContent = showMassage;
+  topOrBottom(formData);
+};
